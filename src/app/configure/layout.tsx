05d793069fb6ba69fb023569f6e608e3e48c0b72
Chirag Tekani <chiragtekani39@gmail.com>
2024-07-20 19:26:15 +0530

    first commit

---
+++ src/app/configure/layout.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import React from "react";
+import Steps from "@/components/Steps";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <MaxWidthWrapper className="flex-1 flex flex-col">
+      <Steps />
+      {children}
+    </MaxWidthWrapper>
+  );
+};
+
+export default Layout;
